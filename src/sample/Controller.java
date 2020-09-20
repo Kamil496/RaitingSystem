3c0481766f528c97a1775591abbb8f43d5532061
Kamil <kamil.smolinski94@gmail.com>
2020-09-20 14:58:11 +0200

    Berechnung des Ratingwerts

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,107 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    private Label txtLable,
+            lblRaiting,
+            lblWertung;
+    @FXML
+    private TextField txtfieldEinnahmen,
+            txtFieldAusgaben;
+
+/*
+Hier werden aus den eingegebenen Parametern ein Bilanzwert berechnet
+ */
+    private Integer berechnungBilanzwert() {
+        String einnahmen = txtfieldEinnahmen.getText();
+        String ausgaben = txtFieldAusgaben.getText();
+        Integer überschussORFehlbetrag = Integer.parseInt(einnahmen) - Integer.parseInt(ausgaben);
+
+        return überschussORFehlbetrag;
+    }
+
+    /*
+    Hier wird das Raiting lvl berechnet und die jeweilige Bweretung festlegelgt
+     */
+    private Integer raiting() {
+        Integer zwichenwert = berechnungBilanzwert() % 5;
+        Integer raiting = zwichenwert + 1;
+        lblRaiting.setText(raiting.toString());
+
+        switch (raiting) {
+            case 15: {
+                lblWertung.setText("Sehr schlecht");
+            }
+            case 14: {
+                lblWertung.setText("Sehr schlecht");
+            }
+            case 13: {
+                lblWertung.setText("Sehr schlecht");
+            }
+            case 12: {
+                lblWertung.setText("Sehr schlecht");
+            }
+            case 11: {
+                lblWertung.setText("Sehr schlecht");
+            }
+            case 10: {
+                lblWertung.setText("Sehr schlecht");
+            }
+            case 9: {
+                lblWertung.setText("Okay");
+            }
+            case 8: {
+                lblWertung.setText("Okay");
+            }
+            case 7: {
+                lblWertung.setText("Okay");
+            }
+            case 6: {
+                lblWertung.setText("Okay");
+            }
+            case 5: {
+                lblWertung.setText("Sehr gut");
+            }
+            case 4: {
+                lblWertung.setText("Sehr gut");
+            }
+            case 3: {
+                lblWertung.setText("Sehr gut");
+            }
+            case 2: {
+                lblWertung.setText("Sehr gut");
+            }
+            case 1: {
+                lblWertung.setText("Sehr gut");
+            }
+        }
+
+        if (raiting >= 15) {
+            raiting = 15;
+            lblWertung.setText("Sehr Schlecht");
+        }
+        if (raiting <= 1) {
+            raiting = 1;
+            lblWertung.setText("Sehr gut");
+        }
+
+        return raiting;
+
+    }
+
+    /*
+    Button zum auslösen der Berechnung
+     */
+    public void calculate() {
+
+        raiting();
+
+    }
+
+
 }
