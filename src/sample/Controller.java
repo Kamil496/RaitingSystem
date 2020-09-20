03d64028901468f853bff7c9603ed1dfcedd25d4
Kamil <kamil.smolinski94@gmail.com>
2020-09-20 18:08:27 +0200

    english version

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -9,20 +9,20 @@ public class Controller {
     @FXML
     private Label txtLable,
             lblRaiting,
-            lblWertung;
+            lblValue;
     @FXML
-    private TextField txtfieldEinnahmen,
-            txtFieldAusgaben;
+    private TextField txtIncome,
+            txtEditions;
 
     /*
     Hier werden aus den eingegebenen Parametern ein Bilanzwert berechnet
      */
-    private Integer berechnungBilanzwert() {
-        String einnahmen = txtfieldEinnahmen.getText();
-        String ausgaben = txtFieldAusgaben.getText();
-        Integer überschussORFehlbetrag = Integer.parseInt(einnahmen) - Integer.parseInt(ausgaben);
+    private Integer calculateValue() {
+        String income = txtIncome.getText();
+        String editions = txtEditions.getText();
+        Integer excessORdeficit = Integer.parseInt(income) - Integer.parseInt(editions);
 
-        return überschussORFehlbetrag;
+        return excessORdeficit;
     }
 
 
@@ -30,66 +30,66 @@ public class Controller {
     Hier wird das Raiting lvl berechnet und die jeweilige Bweretung festlegelgt
      */
     private Integer raiting() {
-        Integer zwichenwert = berechnungBilanzwert() % 5;
-        Integer raiting = zwichenwert + 1;
+        Integer tempValue = calculateValue() % 5;
+        Integer raiting = tempValue + 1;
         lblRaiting.setText(raiting.toString());
 
         switch (raiting) {
             case 15: {
-                lblWertung.setText("Sehr schlecht");
+                lblValue.setText("Sehr schlecht");
             }
             case 14: {
-                lblWertung.setText("Sehr schlecht");
+                lblValue.setText("Sehr schlecht");
             }
             case 13: {
-                lblWertung.setText("Sehr schlecht");
+                lblValue.setText("Sehr schlecht");
             }
             case 12: {
-                lblWertung.setText("Sehr schlecht");
+                lblValue.setText("Sehr schlecht");
             }
             case 11: {
-                lblWertung.setText("Sehr schlecht");
+                lblValue.setText("Sehr schlecht");
             }
             case 10: {
-                lblWertung.setText("Sehr schlecht");
+                lblValue.setText("Sehr schlecht");
             }
             case 9: {
-                lblWertung.setText("Okay");
+                lblValue.setText("Okay");
             }
             case 8: {
-                lblWertung.setText("Okay");
+                lblValue.setText("Okay");
             }
             case 7: {
-                lblWertung.setText("Okay");
+                lblValue.setText("Okay");
             }
             case 6: {
-                lblWertung.setText("Okay");
+                lblValue.setText("Okay");
             }
             case 5: {
-                lblWertung.setText("Sehr gut");
+                lblValue.setText("Sehr gut");
             }
             case 4: {
-                lblWertung.setText("Sehr gut");
+                lblValue.setText("Sehr gut");
             }
             case 3: {
-                lblWertung.setText("Sehr gut");
+                lblValue.setText("Sehr gut");
             }
             case 2: {
-                lblWertung.setText("Sehr gut");
+                lblValue.setText("Sehr gut");
             }
             case 1: {
-                lblWertung.setText("Sehr gut");
+                lblValue.setText("Sehr gut");
             }
         }
 
         if (raiting >= 15) {
             raiting = 15;
-            lblWertung.setText("Sehr Schlecht");
+            lblValue.setText("Sehr Schlecht");
         }
         if (raiting <= 1) {
             raiting = 1;
             lblRaiting.setText("1");
-            lblWertung.setText("Sehr gut");
+            lblValue.setText("Sehr gut");
         }
 
         return raiting;
@@ -99,8 +99,8 @@ public class Controller {
     /*
     Prüfen ob Textfelder leer sind
      */
-    private void checkIfEmpty(){
-        if (txtFieldAusgaben.getText().isEmpty() || txtfieldEinnahmen.getText().isEmpty()) {
+    private void checkIfEmpty() {
+        if (txtEditions.getText().isEmpty() || txtIncome.getText().isEmpty()) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Bitte einen Wert eingeben");
             alert.showAndWait();
@@ -111,9 +111,8 @@ public class Controller {
     Button zum auslösen der Berechnung
      */
     public void calculate() {
+
         checkIfEmpty();
         raiting();
     }
-
-
 }
